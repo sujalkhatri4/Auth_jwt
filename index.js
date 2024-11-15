ae6daf7fa42142d64d7f146a8568bea7934f6d6c
Sujal <sujalkhatri411@gmail.com>
2024-11-15 00:28:47 -0500

    updated index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+//const routes = require('./routes/routes');
+const server = http.createServer(routes);
+require('dotenv').config();
+
+const { API_PORT} = process.env;
+const port = process.env.PORT || API_PORT;
+
+server.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
+
